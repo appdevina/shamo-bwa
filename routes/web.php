de52e7ecc0e88e64dcf3e73ac103cfc3409b9ea2
appdevina <devina@completeselular.com>
2023-01-03 10:02:26 +0700

    CRUD Kategori Produk - Index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,17 +14,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
+    Route::name('dashboard.')->prefix('dashboard')->group(function() {
+        Route::get('/', [DashboardController::class, 'index'])->name(('index'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified','admin',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+        Route::middleware(['admin'])->group(function () {
+            Route::resource('category', ProductCategoryController::class);
+        });
+    });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        '.app/http/controllers/**/*.php',
     ],
 
     theme: {
